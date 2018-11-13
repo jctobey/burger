abc4043f183a9990353ea5c598ae391d35e1bfe7
James Tobey <jctobey@eckerd.edu>
2018-11-13 12:07:18 -0600

    test slack

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ INSERT INTO burgers (burger_name, devoured) VALUES ("Bueno Burger", 0)
 INSERT INTO burgers (burger_name, devoured) VALUES ("Blacksmith Burger", 0)
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bac'n Cheezburger", 0)
 
-
+testing github to see if y'all get notifications when I push to my repo
 
